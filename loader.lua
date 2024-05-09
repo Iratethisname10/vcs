8babe7243359e52e7b86eee20463e3c262abbc74
IRateThisName10 <115908538+Iratethisname10@users.noreply.github.com>
2024-05-09 14:40:26 +0800

    Add files via upload

--- loader.lua
+++ loader.lua
@@ -87,13 +87,7 @@ do
 	local antiPreloadAsync = readfile('Unbounded Yield V2/bypasses/preloadAsync.bin') == 'true'
 
 	if bypassAdonis then
-		if getgenv().bypassedAdonis then return end
-		getgenv().bypassedAdonis = true
-		for _, v in getgc(true) do
-			if pcall(function() return rawget(v, 'indexInstance') end) and type(rawget(v, 'indexInstance')) == 'table' and (rawget(v, 'indexInstance'))[1] == 'kick' then
-				v.tvk = {'kick', function() return workspace:waitForChild('') end}
-			end
-		end
+
 	end
 
 	if spoofMemory then
