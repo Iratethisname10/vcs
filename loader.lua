25f34a9a30f788676edb4e7d545d041c2d7ffb2e
IRateThisName10 <115908538+Iratethisname10@users.noreply.github.com>
2024-07-22 15:53:55 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,2 +1,2 @@
-game.Players.LocalPlayer:kick('script is not working as of now, sorry. discord inv has been copied to your clipboard');
+game.Players.LocalPlayer:Kick('script is not working as of now, sorry. discord inv has been copied to your clipboard');
 setclipboard('https://discord.gg/sRz4eEs9Qk');
